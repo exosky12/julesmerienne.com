108233faba35ede403e2b5f1309a771dcf6a0e96
jumerienne <jules.merienne@etu.uca.fr>
2025-12-15 13:24:25 +0100

    feat: defer PostHog initialization, move font import to layout, and update `myself.png`.

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -6,7 +6,6 @@ import { hydrateRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 import { Layout } from '~/components/layout/layout'
-import posthog from 'posthog-js'
 import { LanguageProvider } from '~/context/LanguageContext'
 
 if (typeof window !== 'undefined') {
@@ -15,11 +14,22 @@ if (typeof window !== 'undefined') {
     console.warn(
       'PostHog API Key is missing! Check your VITE_POSTHOG_API_KEY environment variable.'
     )
+  } else {
+    const initPostHog = async () => {
+      const { default: posthog } = await import('posthog-js')
+      posthog.init(apiKey, {
+        api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only',
+      })
+    }
+
+    // Defer initialization
+    if ('requestIdleCallback' in window) {
+      ;(window as any).requestIdleCallback(initPostHog)
+    } else {
+      setTimeout(initPostHog, 2000)
+    }
   }
-  posthog.init(apiKey, {
-    api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://eu.i.posthog.com',
-    person_profiles: 'identified_only',
-  })
 }
 
 export type InertiaPage = Function & { layout?: (element: React.JSX.Element) => React.JSX.Element }
